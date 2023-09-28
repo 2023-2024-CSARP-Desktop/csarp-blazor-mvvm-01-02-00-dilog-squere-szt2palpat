58c6896080fbd7d4ed7e77c165cb535396d92ef0
szt2palpat <144673180+szt2palpat@users.noreply.github.com>
2023-09-28 09:55:11 +0200

    dun

--- Squere/Squere/Client/ViewModels/ISquereViewmodel.cs
+++ Squere/Squere/Client/ViewModels/ISquereViewmodel.cs
@@ -1,6 +1,13 @@
-﻿namespace Squere.Client.ViewModels
+﻿using ViewModels.BaseClass;
+
+namespace SquareProject.Client.ViewModels
 {
-    public interface ISquereViewmodel
+    public interface ISquareViewModel : IViewModelBase
     {
+        public string Side { get; set; }
+        public string Perimeter { get; }
+        public string Area { get; }
+
+        public void Compute();
     }
 }
